fcaf85dcec012bb45df6bc3983744df8c5cb25f3
larry huang <larryhuang@larryhuangs-MacBook-Pro.local>
2019-12-14 21:57:17 +0800

    feat:binary_search

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BinarySearch
+{
+    public class BinarySearch
+    {
+        int findNum = 0, resultNum = -1;
+        int[] nums;
+        int time = 0;
+        double totalMS = 0;
+
+        public BinarySearch(int numCount)
+        {
+            nums = new int[numCount];
+            for (int i = 0; i < numCount; i++)
+                nums[i] = i;
+        }
+
+        public void Search(int num)
+        {
+            findNum = num;
+
+            Stopwatch st = new Stopwatch();
+            st.Start();
+
+            SearchNum(0, nums.Length);
+
+            st.Stop();
+            totalMS = st.Elapsed.TotalMilliseconds;
+        }
+
+        void SearchNum(int startIndex, int endIndex)
+        {
+            time++;
+            int midIndex = (startIndex + endIndex) / 2;
+            resultNum = nums[midIndex];
+
+
+            if (resultNum == findNum)
+                return;
+            else if (resultNum > findNum)
+                SearchNum(startIndex, midIndex);
+            else
+                SearchNum(midIndex, endIndex);
+        }
+
+
+
+        public override string ToString()
+            => $@"search type: {nameof(BinarySearch)}, search times: {time}, time spends: {totalMS}ms.";
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int numCount = 1000000;
+
+            var binarySearch = new BinarySearch(numCount);
+            binarySearch.Search(new Random().Next(0, numCount));
+            Console.WriteLine(binarySearch.ToString());
+
+            Console.ReadLine();
+
+        }
+    }
+}
