adaa6f484dfb128b86ebcde51de468e87f65211a
xlano <xlano@sina.com>
2019-12-25 10:48:30 +0800

    'feat:DeepFirstSearch'

---
+++ DFS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DFS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deepFirstSearch = new DeepFirstSearch();
+            deepFirstSearch.Search("bonnie", "Design");
+            Console.WriteLine(deepFirstSearch.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
